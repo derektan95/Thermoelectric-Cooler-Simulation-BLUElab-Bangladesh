36a5a2ce3ab3544eaefd1d92544f0b3cee6011fd
derektan <derektan@umich.edu>
2020-03-17 16:01:04 -0400

    Modified CFM on cold side to 33 CFM because we found a 8x8 fan that is better.

--- own_app/input_current_analysis/param_thermoelectric_cooling.m
+++ own_app/input_current_analysis/param_thermoelectric_cooling.m
@@ -64,20 +64,20 @@ R_k_hc = 1 / ( num_semi_cond * (width_semi_cond^2/height_semi_cond) * (k_bismuth
 % k_fin_cold = 237;                % Conduction Coeff - Aluminum [W/mK]
 
 % % Longer Length FIN (Fin 4)
-% fin_width_cold = 0.15;           % length parallel to flow [m]
-% fin_length_cold = 0.03125;             % CHANGEME
-% fin_thickness_cold = 0.000834;          % CHANGEME
-% sink_height_cold = 0.069;            % CHANGEME
-% num_fins_cold = 27;               % CHANGEME
-% k_fin_cold = 237;                % Conduction Coeff - Aluminum [W/mK]
-
-% % Shorter Length FIN (Fin 5)
-fin_width_cold = 0.10;           % length parallel to flow [m]
+fin_width_cold = 0.15;           % length parallel to flow [m]
 fin_length_cold = 0.03125;             % CHANGEME
 fin_thickness_cold = 0.000834;          % CHANGEME
 sink_height_cold = 0.069;            % CHANGEME
 num_fins_cold = 27;               % CHANGEME
 k_fin_cold = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+% % Shorter Length FIN (Fin 5)
+% fin_width_cold = 0.10;           % length parallel to flow [m]
+% fin_length_cold = 0.03125;             % CHANGEME
+% fin_thickness_cold = 0.000834;          % CHANGEME
+% sink_height_cold = 0.069;            % CHANGEME
+% num_fins_cold = 27;               % CHANGEME
+% k_fin_cold = 237;                % Conduction Coeff - Aluminum [W/mK]
 
 per_fin_area_cold = 2 * fin_width_cold * fin_length_cold;
 base_area_cold = (fin_width_cold * sink_height_cold) - (num_fins_cold * fin_width_cold * fin_thickness_cold);  
@@ -97,13 +97,21 @@ fin_area_total_cold = ( (num_fins_cold-1) * per_fin_area_cold) + base_area_cold;
 % num_fins_hot = 12;                                % CHANGEME
 % k_fin_hot = 237;                                 % Conduction Coeff - Aluminum [W/mK]
     
-% Fin 5
-fin_width_hot = 0.10;           % length parallel to flow [m]
+% Longer Length FIN (Fin 4)
+fin_width_hot = 0.15;           % length parallel to flow [m]
 fin_length_hot = 0.03125;             % CHANGEME
 fin_thickness_hot = 0.000834;          % CHANGEME
 sink_height_hot = 0.069;            % CHANGEME
 num_fins_hot = 27;               % CHANGEME
 k_fin_hot = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+% % Fin 5
+% fin_width_hot = 0.10;           % length parallel to flow [m]
+% fin_length_hot = 0.03125;             % CHANGEME
+% fin_thickness_hot = 0.000834;          % CHANGEME
+% sink_height_hot = 0.069;            % CHANGEME
+% num_fins_hot = 27;               % CHANGEME
+% k_fin_hot = 237;                % Conduction Coeff - Aluminum [W/mK]
 
 
 per_fin_area_hot = 2 * fin_width_hot * fin_length_hot;                              
--- own_app/input_current_analysis/thermoelectric_calc.m
+++ own_app/input_current_analysis/thermoelectric_calc.m
@@ -20,7 +20,7 @@ J_max = 8.0;
 
 % Initial conditions - Cold Side (Air restricted to channel)
 inlet_temp_cold = 308.15;   % K
-CFM_nominal_cold = 43;                           % Nominal from specsheet (Small Fan = 5.8579, Large Fan = 59)
+CFM_nominal_cold = 33;                           % Nominal from specsheet (Small Fan = 5.8579, Large Fan = 59)
 input_voltage_adjust_factor = 2;                                % Divide CFM by voltage divident
 CFM_fan_cold = CFM_nominal_cold / input_voltage_adjust_factor;       % CubicFt/min (CFM_max = 5.8579)
 volumetric_flow_rate_cold = CFM_fan_cold * ((0.3048^3) / 60);   % m^3/s - conversion factor
@@ -35,10 +35,10 @@ m_dot_air_cold_per_channel = Area_cross_sect_cold_per_channel * rho_air * air_sp
 
 % Initial conditions - Hot Side (Air not restricted to channel)
 inlet_temp_hot = 308.15;   % K
-CFM_fan_hot = 48;              % CubicFt/min
+CFM_fan_hot = 59;              % CubicFt/min
 volumetric_flow_rate_hot = CFM_fan_hot * ((0.3048^3) / 60);   % m^3/s - conversion factor
 m_dot_air_hot = volumetric_flow_rate_hot / rho_air;
-fan_area_hot = pi * 0.04^2;                                       % CHANGEME
+fan_area_hot = (pi * 0.04^2 - pi * 0.015^2);                                       % CHANGEME
 air_speed_hot = volumetric_flow_rate_hot / fan_area_hot ;         % m/s
 
 % Compute convective coefficient & fin efficiencies
