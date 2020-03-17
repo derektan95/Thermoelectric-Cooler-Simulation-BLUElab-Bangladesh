4ab9acb8db9f99e8b88104f2c9d27d592ec569d2
derektan <derektan@umich.edu>
2020-03-17 16:41:15 -0400

    Added new TEC1-12706 configuration to other scripts.

--- own_app/fin_dimension_analysis/param_thermoelectric_cooling_dimension_analysis.m
+++ own_app/fin_dimension_analysis/param_thermoelectric_cooling_dimension_analysis.m
@@ -22,6 +22,18 @@ rho_air = 1.177;                     % Density (Kg/m^3)
 
 
 % Table C.9 - Bismuth Telluride Peltier element properties (TEC1-12706)
+% Match spec sheet and physical dimensions
+alpha_s_pos = 1.25 * 10^-4;           % V/degC
+alpha_s_neg = -1.25 * 10^-4;          % V/degC
+rho_e_pos = 0.28 * 10^-5;                   % Ohm-m
+rho_e_neg = rho_e_pos;               % Ohm-m
+k_bismuth_pos = 2.0;                 % W/mK
+k_bismuth_neg = 2.0;                % W/mK
+width_semi_cond = 0.0011;            % m 
+height_semi_cond = 0.0022;           % m
+num_semi_cond = 220;                 % 127 couples
+
+% % OLDEST
 % alpha_s_pos = 2.3 * 10^-4;           % V/degC
 % alpha_s_neg = -2.1 * 10^-4;          % V/degC
 % rho_e_pos = 10^-5;                   % Ohm-m
@@ -32,15 +44,16 @@ rho_air = 1.177;                     % Density (Kg/m^3)
 % height_semi_cond = 0.0035;           % m
 % num_semi_cond = 254;                 % 127 couples
 
-alpha_s_pos = 1.05 * 10^-4;           % V/degC
-alpha_s_neg = -1.05 * 10^-4;          % V/degC
-rho_e_pos = 0.5 * 10^-5;                   % Ohm-m
-rho_e_neg = rho_e_pos;               % Ohm-m
-k_bismuth_pos = 0.8;                 % W/mK
-k_bismuth_neg = 0.8;                % W/mK
-width_semi_cond = 0.0018;            % m (NOT TOO SURE..)
-height_semi_cond = 0.0027;           % m
-num_semi_cond = 254;                 % 127 couples
+% % OLD
+% alpha_s_pos = 1.05 * 10^-4;           % V/degC
+% alpha_s_neg = -1.05 * 10^-4;          % V/degC
+% rho_e_pos = 0.5 * 10^-5;                   % Ohm-m
+% rho_e_neg = rho_e_pos;               % Ohm-m
+% k_bismuth_pos = 0.8;                 % W/mK
+% k_bismuth_neg = 0.8;                % W/mK
+% width_semi_cond = 0.0018;            % m (NOT TOO SURE..)
+% height_semi_cond = 0.0027;           % m
+% num_semi_cond = 254;                 % 127 couples
 
 alpha_seeback = alpha_s_pos - alpha_s_neg;
 R_e_hc = (height_semi_cond/(width_semi_cond^2)) * (rho_e_pos + rho_e_neg);
