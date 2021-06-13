df243bff84fa9a8e471b501a2f87c60759bb120f
derektan <derektan@umich.edu>
2021-06-13 20:44:31 +0800

    Reorganized folders where input current analysis folders of different config added to a common folder.

---
+++ cooler_project_bangladesh/input_current_analysis/hybrid_config_input_current_analysis/hybrid_config_param_thermoelectric_cooling.m
@@ -0,0 +1,183 @@
+%% Properties for calculation
+
+clear global;
+clc;clear;
+
+%% Declare variables as global for use in other scripts (bad practice)
+global kin_visc_air Cp_air k_air alpha_air Pr_air rho_air 
+global Area_cross_sect_cold_per_channel Dh_cold_per_channel num_channels area_per_channel
+global R_e_hc R_k_hc alpha_seeback num_semi_cond
+global fin_width_cold fin_length_cold fin_thickness_cold sink_height_cold num_fins_cold k_fin_cold per_fin_area_cold base_area_cold fin_area_total_cold
+global fin_width_hot fin_length_hot fin_thickness_hot sink_height_hot num_fins_hot k_fin_hot per_fin_area_hot base_area_hot fin_area_total_hot 
+
+%% Properties
+
+% Table C.22 (300K) - Air properties
+kin_visc_air = 1.566 * 10^-5;        % Calculate Reynolds (m^2/s)
+Cp_air = 1005;                       % heat capacity (J/kgK)
+k_air = 0.0267;                      % To calculate Conv Coeff (W/mK)
+alpha_air = 2.257 * 10^-5;           % To calculate Le (m^2/s)
+Pr_air = 0.69;                       % Prandt Num ()
+rho_air = 1.177;                     % Density (Kg/m^3)
+
+
+% % Table C.9 - Bismuth Telluride Peltier element properties (TEC1-12706) -
+% % % match Spec Sheet...
+% % MORE ACCURATE 
+% alpha_s_pos = 2.5 * 10^-4;           % V/degC
+% alpha_s_neg = -2.35 * 10^-4;          % V/degC
+% rho_e_pos = 0.5 * 10^-5;                   % Ohm-m
+% rho_e_neg = rho_e_pos;               % Ohm-m
+% k_bismuth_pos = 4.2;                 % W/mK
+% k_bismuth_neg = 4.2;                % W/mK
+% width_semi_cond = 0.00108;            % m 
+% height_semi_cond = 0.0022;           % m
+% num_semi_cond = 110;                 % 110 couples
+
+% % LESS ACCURATE (TEC1-12706)
+% alpha_s_pos = 1.25 * 10^-4;           % V/degC
+% alpha_s_neg = -1.25 * 10^-4;          % V/degC
+% rho_e_pos = 0.28 * 10^-5;                   % Ohm-m
+% rho_e_neg = rho_e_pos;               % Ohm-m
+% k_bismuth_pos = 2.0;                 % W/mK
+% k_bismuth_neg = 2.0;                % W/mK
+% width_semi_cond = 0.0011;            % m 
+% height_semi_cond = 0.0022;           % m
+% num_semi_cond = 220;                 % 110 couples (SHOULD ACTUALLY USE 110 instead of 220)
+
+
+% % USED FOR MORE POWER BUT MORE INPUT CURRENT...
+
+% % MORE ACCURATE
+% % Bismuth Telluride Peltier element properties (TEC1-12710)
+alpha_s_pos = 2.3 * 10^-4;           % V/degC
+alpha_s_neg = -2.2 * 10^-4;          % V/degC
+rho_e_pos = 0.55 * 10^-5;                   % Ohm-m
+rho_e_neg = rho_e_pos;               % Ohm-m
+k_bismuth_pos = 3.45;                 % W/mK
+k_bismuth_neg = 3.45;                % W/mK
+width_semi_cond = 0.00135;            % m 
+height_semi_cond = 0.0019;           % m
+num_semi_cond = 126;                 % 127 couples
+
+% % Bismuth Telluride Peltier element properties (TEC1-12710)
+% alpha_s_pos = 1.15 * 10^-4;           % V/degC
+% alpha_s_neg = -1.1 * 10^-4;          % V/degC
+% rho_e_pos = 0.28 * 10^-5;                   % Ohm-m
+% rho_e_neg = rho_e_pos;               % Ohm-m
+% k_bismuth_pos = 1.7;                 % W/mK
+% k_bismuth_neg = 1.65;                % W/mK
+% width_semi_cond = 0.00135;            % m 
+% height_semi_cond = 0.0019;           % m
+% num_semi_cond = 252;                 % 126 couples (SHOULD ACTUALLY USE 126 instead of 252)
+
+alpha_seeback = alpha_s_pos - alpha_s_neg;
+R_e_hc = (height_semi_cond/(width_semi_cond^2)) * (rho_e_pos + rho_e_neg);
+R_k_hc = 1 / ( num_semi_cond * (width_semi_cond^2/height_semi_cond) * (k_bismuth_pos + k_bismuth_neg) );
+
+
+%% Fin conditions - Cold Side      
+
+% % SMALL COLD FIN (Fin 1)
+% fin_width_cold = 0.045;           % length parallel to flow [m]
+% fin_length_cold = 0.021;             % CHANGEME
+% fin_thickness_cold = 0.001;          % CHANGEME
+% sink_height_cold = 0.04;            % CHANGEME
+% num_fins_cold = 9;               % CHANGEME
+% k_fin_cold = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+% % LARGE COLD FIN (Same as on hot side) - (Fin 2)
+% fin_width_cold = 0.12;                % length parallel to flow [m]
+% fin_length_cold = 0.027;                              % CHANGEME
+% fin_thickness_cold = 0.001;                           % CHANGEME
+% sink_height_cold = 0.08;                             % CHANGEME
+% num_fins_cold = 12;                                % CHANGEME
+% k_fin_cold = 237;                                 % Conduction Coeff - Aluminum [W/mK]
+
+% % Moderate Length FIN (Fin 3)
+% fin_width_cold = 0.13;           % length parallel to flow [m]
+% fin_length_cold = 0.03125;             % CHANGEME
+% fin_thickness_cold = 0.0007;          % CHANGEME
+% sink_height_cold = 0.0693;            % CHANGEME
+% num_fins_cold = 27;               % CHANGEME
+% k_fin_cold = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+% % Longer Length FIN (Fin 4)
+fin_width_cold = 0.15;           % length parallel to flow [m]
+fin_length_cold = 0.03125;             % CHANGEME
+fin_thickness_cold = 0.0007;          % CHANGEME
+sink_height_cold = 0.0693;            % CHANGEME
+num_fins_cold = 27;               % CHANGEME
+k_fin_cold = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+% % Shorter Length FIN (Fin 5) - Esp for 2 stage analysis
+% fin_width_cold = 0.10;           % length parallel to flow [m]
+% fin_length_cold = 0.03125;             % CHANGEME
+% fin_thickness_cold = 0.0007;          % CHANGEME
+% sink_height_cold = 0.0693;            % CHANGEME
+% num_fins_cold = 27;               % CHANGEME
+% k_fin_cold = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+per_fin_area_cold = 2 * fin_width_cold * fin_length_cold;
+base_area_cold = (fin_width_cold * sink_height_cold) - (num_fins_cold * fin_width_cold * fin_thickness_cold);  
+fin_area_total_cold = ( (num_fins_cold-1) * per_fin_area_cold) + base_area_cold;
+% % fin_area_total_cold = 0.05;      % Given by prof's example [m^2]
+
+  
+
+
+%% Fin conditions - Hot Side     
+
+% % Defualt hot fins
+% fin_width_hot = 0.12;                % length parallel to flow [m]
+% fin_length_hot = 0.027;                              % CHANGEME
+% fin_thickness_hot = 0.001;                           % CHANGEME
+% sink_height_hot = 0.08;                             % CHANGEME
+% num_fins_hot = 12;                                % CHANGEME
+% k_fin_hot = 237;                                 % Conduction Coeff - Aluminum [W/mK]
+    
+% % Longer Length FIN (Fin 4)
+% fin_width_hot = 0.15;           % length parallel to flow [m]
+% fin_length_hot = 0.03125;             % CHANGEME
+% fin_thickness_hot = 0.0007;          % CHANGEME
+% sink_height_hot = 0.0693;            % CHANGEME
+% num_fins_hot = 27;               % CHANGEME
+% k_fin_hot = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+% % Fin 5
+% fin_width_hot = 0.10;           % length parallel to flow [m]
+% fin_length_hot = 0.03125;             % CHANGEME
+% fin_thickness_hot = 0.0007;          % CHANGEME
+% sink_height_hot = 0.0693;            % CHANGEME
+% num_fins_hot = 27;               % CHANGEME
+% k_fin_hot = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+% % Cut Fin 4 (150mm) into half
+fin_width_hot = 0.075;           % length parallel to flow [m]
+fin_length_hot = 0.03125;             % CHANGEME
+fin_thickness_hot = 0.0007;          % CHANGEME
+sink_height_hot = 0.0693;            % CHANGEME
+num_fins_hot = 27;               % CHANGEME
+k_fin_hot = 237;                % Conduction Coeff - Aluminum [W/mK]
+
+
+per_fin_area_hot = 2 * fin_width_hot * fin_length_hot;                              
+base_area_hot = (fin_width_hot * sink_height_hot) - (num_fins_hot * fin_width_hot * fin_thickness_hot); 
+fin_area_total_hot = ( (num_fins_hot-1) * per_fin_area_hot) + base_area_hot;
+% fin_area_total_hot = fin_area_total_cold*4;      % Given by prof's example [m^2]  
+
+
+%% Dimensions of flow channel (between sheets)
+% According to size of heat sink on cold side... (account for area covered
+% by fins)
+
+% width = sink_height_cold;
+height = fin_length_cold;
+num_channels = num_fins_cold - 1;
+width_btwn_fins = (sink_height_cold - (num_fins_cold * fin_thickness_cold) )/num_channels;
+Area_cross_sect_cold_per_channel = (height * width_btwn_fins);
+perimeter_per_channel = (2 * height) + (2 * width_btwn_fins);
+Dh_cold_per_channel = 4*Area_cross_sect_cold_per_channel/perimeter_per_channel; 
+
+area_per_channel = fin_area_total_cold / num_channels;
+
---
+++ cooler_project_bangladesh/input_current_analysis/hybrid_config_input_current_analysis/hybrid_config_thermoelectric_calc.m
@@ -0,0 +1,275 @@
+%% Load essential parameters
+% Implement struct data structure in the future!
+
+% warning('off','all');           % Turn off all warnings
+run("hybrid_config_param_thermoelectric_cooling.m");
+
+%% Declare variables as global for use in other scripts (bad practice)
+global kin_visc_air Cp_air k_air alpha_air Pr_air rho_air 
+global Area_cross_sect_cold_per_channel Dh_cold_per_channel num_channels area_per_channel
+global R_e_hc R_k_hc alpha_seeback num_semi_cond
+global fin_width_cold fin_length_cold fin_thickness_cold sink_height_cold num_fins_cold k_fin_cold per_fin_area_cold base_area_cold fin_area_total_cold
+global fin_width_hot fin_length_hot fin_thickness_hot sink_height_hot num_fins_hot k_fin_hot per_fin_area_hot base_area_hot fin_area_total_hot 
+
+%% Define simulation parameters (CHANGME)
+
+% Number of stages (for peltier modules in series)
+num_modules_series = 2;
+
+% General parameters
+J_e = 0;              % Optimal current (CHANGE TO FUNCTION)
+J_iters = 100;
+J_max = 10.0;
+
+% Initial conditions - Cold Side (Air restricted to channel)
+inlet_temp_cold = 308.15;   % K
+CFM_nominal_cold = 33;                           % Nominal from specsheet (Small Fan = 5.8579, Large Fan = 59)
+input_voltage_adjust_factor = 1;                                % Divide CFM by voltage divident
+CFM_fan_cold = CFM_nominal_cold / input_voltage_adjust_factor;       % CubicFt/min (CFM_max = 5.8579)
+volumetric_flow_rate_cold = CFM_fan_cold * ((0.3048^3) / 60);   % m^3/s - conversion factor
+m_dot_air_cold = volumetric_flow_rate_cold * rho_air;
+% fan_area_cold = pi * 0.02^2;                                   % CHANGEME
+total_cross_section_fin_area = Area_cross_sect_cold_per_channel * num_channels;
+air_speed_cold = volumetric_flow_rate_cold / total_cross_section_fin_area ;         % m/s
+
+% air_speed_cold = 2.2;      % m/s
+m_dot_air_cold_per_channel = Area_cross_sect_cold_per_channel * rho_air * air_speed_cold;
+% m_dot_air_cold_per_channel_1 = m_dot_air_cold / num_channels;
+
+% Initial conditions - Hot Side (Air not restricted to channel)
+inlet_temp_hot = 308.15;   % K
+CFM_fan_hot = 59;              % CubicFt/min
+volumetric_flow_rate_hot = CFM_fan_hot * ((0.3048^3) / 60);   % m^3/s - conversion factor
+m_dot_air_hot = volumetric_flow_rate_hot / rho_air;
+fan_area_hot = (pi * 0.04^2 - pi * 0.015^2);                                       % CHANGEME
+air_speed_hot = volumetric_flow_rate_hot / fan_area_hot ;         % m/s
+
+% Compute convective coefficient & fin efficiencies
+[R_ku_cold, h_cold] = compute_convective_coefficient_cold_NTU(air_speed_cold, area_per_channel, fin_width_cold, Dh_cold_per_channel, m_dot_air_cold_per_channel);
+[R_ku_hot, h_hot] = compute_convective_coefficient_hot_without_NTU(air_speed_hot, fin_area_total_hot, fin_width_hot);
+overall_fin_eff_hot = compute_fin_efficiency(h_hot, k_fin_hot, fin_thickness_hot, fin_length_hot, num_fins_hot, per_fin_area_hot, fin_area_total_hot);        
+overall_fin_eff_cold = compute_fin_efficiency(h_cold, k_fin_cold, fin_thickness_cold, fin_length_cold, num_fins_cold, per_fin_area_cold, fin_area_total_cold);        
+
+
+%% Print initialization message
+fprintf('<strong>***Initialization***\n</strong>');
+fprintf('Inlet Air Temperature - Cold Side (T_in_cold): %.3f K \n', inlet_temp_cold);
+fprintf('Inlet Air Speed - Cold Side (U_cold): %.1f m/s \n', air_speed_cold);
+fprintf('Inlet Air Temperature - Hot Side (T_in_hot): %.3f K \n', inlet_temp_hot);
+fprintf('Inlet Air Speed - Hot Side (U_hot): %.1f m/s \n', air_speed_hot);
+fprintf('Convective Coefficient Resistance PER CHANNEL (R_ku_c) - Cold Side: %.3f K/W\n', R_ku_cold);
+fprintf('Convective Coefficient Resistance (R_ku_h) - Hot Side: %.3f K/W\n', R_ku_hot);
+fprintf('Conductive Coefficient Resistance (R_k_hc): %.3f K/W\n\n', R_k_hc);
+
+%% Main Calculation Body
+
+% Initialize data structure for plotting
+cooling_power_arr = zeros(J_iters, 1);
+heating_power_arr = zeros(J_iters, 1);
+power_required_arr = zeros(J_iters, 1);
+outlet_temp_cold_arr = zeros(J_iters, 1);
+COP_arr = zeros(J_iters, 1);
+delta_J_arr = linspace(0, J_max, J_iters);
+
+% Optimal Variables for optimal current
+J_optimal = 0;
+max_cooling_power = 0;
+T_h_optimal = 0;
+T_c_optimal = 0;
+power_required_optimal = 0;
+COP_optimal = 0;
+outlet_temp_cold_optimal = 0;
+outlet_temp_hot_optimal = 0;
+
+for i = 1:length(delta_J_arr)
+    
+    J_e = delta_J_arr(i);
+    inlet_temp_cold = 308.15;       % Re-initialize inlet_temp
+    fprintf('\n<strong>===Iteration %d===\n</strong>', i);
+    fprintf('Input Current (J_e): %.2f A \n\n', J_e);
+    
+    % Accumulated variables for through "num_modules_series" stages
+    cooling_power_total = 0;
+    heating_power_total = 0;
+    power_required_total = 0;
+    
+    % Loop based on number of stages
+    for j = 1:num_modules_series
+        
+        fprintf('<strong>-Stage %d- \n</strong>', j);
+        fprintf('Inlet Air Temperature - Cold Side (T_in_cold): %.3f K \n', inlet_temp_cold);
+    
+        % x = T_h, y = T_c, z = Q_c
+        syms x y z
+        eqn1 = ((x - y) / R_k_hc) + (overall_fin_eff_hot * (x-inlet_temp_hot) / R_ku_hot) == (num_semi_cond * alpha_seeback * J_e * x) + (0.5 * num_semi_cond * R_e_hc * J_e^2); 
+        eqn2 = (-(x - y) / R_k_hc) + z == (-num_semi_cond * alpha_seeback * J_e * y) + (0.5 * num_semi_cond * R_e_hc * J_e^2); 
+        eqn3 = z == (overall_fin_eff_cold * num_channels * (y - inlet_temp_cold) ) / R_ku_cold;
+
+        sol = solve([eqn1, eqn2, eqn3], [x, y, z]);
+        T_h_peltier = double(sol.x);
+        T_c_peltier = double(sol.y);
+        Q_c_peltier = double(sol.z);        % Already factored in cold efficiency and all channels...
+
+        Q_h_peltier = overall_fin_eff_hot * (T_h_peltier - inlet_temp_hot) / R_ku_hot;
+        power_conduction_peltier = (T_h_peltier - T_c_peltier) / R_k_hc;
+        outlet_temp_cold = inlet_temp_cold + ( (Q_c_peltier / num_channels) / (m_dot_air_cold_per_channel * Cp_air) );
+        outlet_temp_hot = inlet_temp_hot + Q_h_peltier/(m_dot_air_hot * Cp_air);
+        power_required = num_semi_cond * ((R_e_hc * J_e^2) + (alpha_seeback * J_e * (T_h_peltier - T_c_peltier)) );
+        coefficient_performance = -100 * Q_c_peltier / power_required;
+        
+        % Increment data for variables tracking overall performance
+        cooling_power_total = cooling_power_total + Q_c_peltier;
+        heating_power_total = heating_power_total + Q_h_peltier;
+        power_required_total = power_required_total + power_required;
+        
+        % Update inlet temperature to outlet temp of previous iteration
+        inlet_temp_cold = outlet_temp_cold;
+        fprintf('Outlet Air Temperature - Cold Side (T_out_cold): %.3f K \n\n', outlet_temp_cold);
+        
+    end
+    
+   % Store overall performance from multi-stage analysis
+    cooling_power_arr(i) = cooling_power_total;
+    heating_power_arr(i) = heating_power_total;
+    power_required_arr(i) = power_required_total;
+    outlet_temp_cold_arr(i) =  outlet_temp_cold - 273.15;
+    
+    % Calculate overall COP
+    COP_total = -(100 * cooling_power_total) / power_required_total;
+    COP_arr(i)= COP_total;
+    
+    % Find optimal current which gives max cooling
+    if -cooling_power_total > -max_cooling_power
+        max_cooling_power = cooling_power_total;
+        max_heating_power = heating_power_total;
+        J_optimal = J_e;
+        T_h_optimal = T_h_peltier;
+        T_c_optimal = T_c_peltier;
+        power_required_optimal = power_required_total;
+        COP_optimal = COP_total;
+        outlet_temp_cold_optimal = outlet_temp_cold;
+        outlet_temp_hot_optimal = outlet_temp_hot;
+    end
+    
+    % Print results..
+    fprintf('<strong>Summary \n</strong>');
+    fprintf('Overall Power Required (P_e): %.1f W\n', power_required_total);
+    fprintf('Coefficient of Performance (COP): %.1f %% \n', COP_total);
+    
+    % Cold side
+    fprintf('---\n');
+    fprintf('Cold side Temperature (T_c): %.1f K \n', T_c_peltier);
+    fprintf('Overall Cooling Power - Cold Side (Q_c_peltier): %.2f W\n', cooling_power_total);
+    fprintf('Outlet Air Temperature - Cold Side (T_out_cold): %.1f K\n', outlet_temp_cold);
+    
+    % Hot side
+    fprintf('---\n');
+    fprintf('Hot side Temperature (T_h): %.1f K \n', T_h_peltier);
+    fprintf('Overall Heating Power - Hot Side (Q_h_peltier): %.2f W\n', heating_power_total);
+    fprintf('Outlet Air Temperature - Hot Side (T_out_hot): %.1f K\n\n', outlet_temp_hot);
+
+end
+
+% Display optimal results
+% outlet_temp_cold_optimal = inlet_temp_cold + max_cooling_power/(m_dot_air_cold * Cp_air);
+
+fprintf('<strong>===Optimal Current Results===\n</strong>');
+
+fprintf('Input Current (J_e): %.2f A \n', J_optimal);
+fprintf('Power Required (P_e): %.1f W\n', power_required_optimal);
+fprintf('Coefficient of Performance (COP): %.1f %% \n', COP_optimal);
+
+% Cold side
+fprintf('---\n');
+fprintf('Cold side Temperature (T_c): %.1f K \n', T_c_optimal);
+fprintf('Cooling Power - Cold Side (Q_c_peltier): %.2f W\n', max_cooling_power);
+fprintf('Outlet Air Temperature - Cold Side (T_out_cold): %.1f K\n', outlet_temp_cold_optimal);
+
+% Hot side
+fprintf('---\n');
+fprintf('Hot side Temperature (T_h): %.1f K \n', T_h_optimal);
+fprintf('Heating Power - Hot Side (Q_h_peltier): %.2f W\n', max_heating_power);
+fprintf('Outlet Air Temperature - Hot Side (T_out_hot): %.1f K\n\n', outlet_temp_hot_optimal);
+
+
+%% Plot final graphs
+
+% Remove data points for high COP for visibility sake
+for i = 1:length(delta_J_arr)
+    if delta_J_arr(i) < 1.3
+        COP_arr(i) = NaN;
+    end
+end
+
+% % Plot graph of Cooling power against Current
+% figure(1)
+% plot(delta_J_arr, cooling_power_arr, delta_J_arr, heating_power_arr, delta_J_arr, power_required_arr, delta_J_arr, COP_arr);
+% title("Power against Input Current");
+% xlabel("Current [A]");
+% ylabel("Power [W]");
+% legend("Cooling Power (Q_c)", "Heating Power (Q_h)", "Power Input", "COP [%]",  "Location", "NorthEast");
+% grid on;
+% set(gca,'FontSize',12)
+
+% Plot abs cooling power and power consumption against current
+
+hold on;
+figure(1)
+plot(delta_J_arr, -cooling_power_arr, delta_J_arr, power_required_arr, delta_J_arr, outlet_temp_cold_arr, delta_J_arr, COP_arr);
+title("Input Current Analysis (TEC1-12710) - " + num_modules_series + " Peltier Modules in Series [" + CFM_nominal_cold + " CFM]");
+xlabel("Current [A]");
+ylabel("Magnitude");
+legend("Cooling Power [W]", "Power Consumed [W]", "Outlet Temp [degC]", "COP [%]", "Location", "NorthEast");
+grid on;
+set(gca,'FontSize',12)
+
+%% Main Functions Used
+
+
+% Forced convection - Re > 2300 = turbulent flow
+% Calculations is per channel...
+function [R_ku, h] = compute_convective_coefficient_cold_NTU(air_speed, Area_fin_total, fin_width, Dh, mass_dot)
+    
+    global kin_visc_air k_air Pr_air Cp_air;
+    Re = (air_speed * Dh)/kin_visc_air;
+    Nu = 0.023 * Re^(4/5) * Pr_air^(0.3);       % n = 0.3
+    NTU = (Area_fin_total * Nu * k_air) / (Dh * mass_dot * Cp_air);
+    heat_transfer_eff = 1 - exp(-NTU);                      % R changes with flow in channel...
+    R_ku = 1 / (mass_dot * Cp_air * heat_transfer_eff);     % Average convective resistance
+%     h = 1 / (R_ku * Area_fin_total);                        % R = 1/hA
+    h = Nu * k_air / Dh;
+    
+%     Re = (air_speed * fin_width)/kin_visc_air;
+%     Nu = 0.664 * Re^(0.5) * Pr_air^(1/3);
+%     R_ku = fin_width/(Area_fin_total * Nu * k_air); 
+%     h = Nu * k_air / fin_width;
+
+end
+
+% Assuming flow over plate (Likely laminar Re < 5 * 10^5)
+function [R_ku, h] = compute_convective_coefficient_hot_without_NTU(air_speed, Area_fin_total, fin_width)
+    
+    global kin_visc_air k_air Pr_air;
+
+    Re = (air_speed * fin_width)/kin_visc_air;
+    Nu = 0.664 * Re^(0.5) * Pr_air^(1/3);    
+    R_ku = fin_width/(Area_fin_total * Nu * k_air); 
+    h = Nu * k_air / fin_width;
+
+end
+
+% Returns fin efficiency overall (including base and fins)
+% Convection Coeff = h (Without considering area)
+% conduction_coeff = k
+function fin_eff_overall = compute_fin_efficiency(convection_coeff, conduction_coeff, fin_thickness, fin_length, num_fins, area_fin, area_total)
+    
+    m = sqrt(2 * convection_coeff / (conduction_coeff * fin_thickness) );
+    L_c = fin_length + fin_thickness/2;
+    fin_eff = (tanh(m*L_c) ) / (m * L_c);
+    
+    fin_eff_overall = 1 - ( (num_fins * area_fin / area_total) * (1 - fin_eff) );
+
+end
+
+
+
